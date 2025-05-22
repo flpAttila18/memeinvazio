0a8760479cf5655d1a3e11c22b4ac8fb2bd6f08d
tanulo <tanulo@barossgabor.local>
2025-05-22 11:01:38 +0200

    kép feltöltés

--- js/newMeme.js
+++ js/newMeme.js
@@ -1,9 +1,13 @@
+//globális 
 const fileUpload = document.querySelector('#fileUpload');
+const btnUpload = document.querySelector(".uploadButton")
 let meme = null ; 
 
-
+//esemnények
 fileUpload.addEventListener('change', selectPic)
+btnUpload.addEventListener('click' , uploadMeme)
 
+//függvények
 async function selectPic(){
     const file = fileUpload.files[0];
     console.log(file);
@@ -18,5 +22,26 @@ async function selectPic(){
             newMeme.style.backgroundRepeat='no-repeat';
         }
         reader.readAsDataURL(file);
+    }
+}
+
+async function uploadMeme(){
+    try {
+        if(meme){
+            const formData = new FormData;
+            formData.append('meme', meme);
+            //console.log(formData);
+
+            const response = await fetch('http://127.0.0.1:3000/api/memes/uploadMeme' , {
+                method :'POST',
+                body: formData,
+                credentials:'include'
+            });
+        }
+        else{
+            alert("Válassz ki egy képet!")
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
